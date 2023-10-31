1dd52cfce0ed81248dbb80dbc4baaf9b2e101f4c
juanguille42-prog <77317575+juanguille42-prog@users.noreply.github.com>
2023-10-31 21:48:24 +0000

    commit

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export const Footer = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">clc
+		<nav className="navbar navbar-expand-lg navbar-dark bg-dark footer-fixed">
       <div className="container-fluid">
         <div className="row text-center text-light justify-content-center w-100">
           <h5 className="text">Copy right © Your Website 2023</h5>
