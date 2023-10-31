a5f1391d0579a9648ba89f5fc3a35baa7ab10af2
OUHernandezAyala <142889499+OUHernandezAyala@users.noreply.github.com>
2023-10-31 16:41:06 +0000

    Adding the cards and modifications to the home for the cards

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import NavBar from './nav.jsx';
+import Card from './card.jsx'
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -8,8 +9,23 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<div><NavBar/></div>
+			<div></div>
+			<div id="cards" className="my-4">
+				<div className="row row-cols-1 row-cols-md-1 g-3">
+					<Card/>
+					<Card/>
+					<Card/>
+					<Card/>
+					<Card/>
+					<Card/>
+					<Card/>
+					<Card/>
+				</div>
+			</div>
+			<div></div>
 		</div>
 	);
 };
 
 export default Home;
+
