8ba2ea0312224449121863ebab4a761fe9a622eb
OUHernandezAyala <142889499+OUHernandezAyala@users.noreply.github.com>
2023-10-31 16:51:30 +0000

    Add Styles to the body of the pag.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,17 +9,19 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<div><NavBar/></div>
-			<div></div>
-			<div id="cards" className="my-4">
-				<div className="row row-cols-1 row-cols-md-1 g-3">
-					<Card/>
-					<Card/>
-					<Card/>
-					<Card/>
-					<Card/>
-					<Card/>
-					<Card/>
-					<Card/>
+			<div className="bodyPage">
+				<div></div>
+				<div id="cards" className="my-4 ">
+					<div className="row row-cols-1 row-cols-md-1 g-3">
+						<Card/>
+						<Card/>
+						<Card/>
+						<Card/>
+						<Card/>
+						<Card/>
+						<Card/>
+						<Card/>
+					</div>
 				</div>
 			</div>
 			<div></div>
